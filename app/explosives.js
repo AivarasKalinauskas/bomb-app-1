1eac264d8efeca5350250af8fb187486753eefae
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2019-08-21 22:09:40 +0300

    adding actions and execute

--- app/explosives.js
+++ app/explosives.js
@@ -1,8 +1,14 @@
 export default class Explosives {
-	constructor(elementId, canvas) {
+	constructor(elementId, canvas, actions) {
 		this.elementId = elementId;
 		this.canvas = canvas;
-		console.log("explosives constructor", this.elementId, this.canvas);
+		this.actions = actions;
+		console.log(
+			"explosives constructor",
+			this.elementId,
+			this.canvas,
+			this.actions
+		);
 
 		this.init();
 	}
@@ -17,5 +23,14 @@ export default class Explosives {
 		el.classList.add("icon");
 		el.innerHTML = svg;
 		return el;
+	}
+
+	execute() {
+		if (this.actions.length) {
+			let action = this.actions.shift();
+			this[action]();
+		} else {
+			console.error("fail: no action left to execute");
+		}
 	}
 }
